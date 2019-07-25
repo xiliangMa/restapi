7492c99cdb71d8c8b09ea49c7bcf877e5fb017f3
xiliangMa <xiliangMa@outlook.com>
2019-07-25 18:00:34 +0800

    add pull k8s image file

---
+++ k8s/install/pull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+images=(kube-proxy:v1.14.0 kube-scheduler:v1.14.0 kube-controller-manager:v1.14.0 kube-apiserver:v1.14.0 etcd:3.3.10 coredns:1.3.1 pause:3.1 )
+for imageName in ${images[@]} ; do
+docker pull xiliangma/$imageName
+docker tag  xiliangma/$imageName k8s.gcr.io/$imageName
+docker rmi  xiliangma/$imageName
